1e96517b7c73b79a7edbb1120edbbd138d4adb6e
wlame001 <wlame001@ucr.edu>
2017-05-10 23:42:04 -0700

    Delete multi_command.sh

--- rshell/tests/multi_command.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./a.out <<< "ls -a || echo helloworld"
-./a.out <<< "lv || echo helloworld"
-./a.out <<< "echo pass1; echo pass2 && echo pass3 || echo fail1 || echo pass4"
-./a.out <<< "ech fail1 || echo pass1 ; echo pass2 && echo pass3 || echo fail2"
